cbd9f55927c3e14855acf5e3093b35b3f8f4bbf7
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-05-16 15:38:48 +0530

    Added: UC3-Third-Simulation

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,16 +1,27 @@
-#!/bin/bash 
+#!/bin/bash
 
 headcount=0
 tailcount=0
-while [ $headcount -lt 11 -a  $tailcount -lt 11 ]
+while [ $headcount -lt 21 -a  $tailcount -lt 21 ]
 do 
 flip=$(( RANDOM%2 ))
 if [ $flip -eq 0 ]
 then
-echo "head wins"
 headcount=$(( $headcount+1 ))
 else
-echo "tail wins"
 tailcount=$(( $tailcount+1 ))
+fi
+if [ $headcount -gt $tailcount ]
+then
+echo "head wins" $headcount
+diff=$(( $headcount - $tailcount ))
+echo "head won by difference" $diff
+elif [ $headcount -lt $tailcount ]
+then
+echo "tail wins" $tailcount
+diff=$(( $tailcount - $headcount ))
+echo "tail won by difference" $diff
+else 
+echo "tie"
 fi
 done
