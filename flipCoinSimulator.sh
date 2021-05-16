6b055421671d9d406eb78a6906356dd1b6be4979
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-05-16 15:35:41 +0530

    Added: UC2-Second-Simulation

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,16 @@
-#!/bin/bash -x
+#!/bin/bash 
 
+headcount=0
+tailcount=0
+while [ $headcount -lt 11 -a  $tailcount -lt 11 ]
+do 
 flip=$(( RANDOM%2 ))
 if [ $flip -eq 0 ]
 then
 echo "head wins"
+headcount=$(( $headcount+1 ))
 else
 echo "tail wins"
+tailcount=$(( $tailcount+1 ))
 fi
+done
